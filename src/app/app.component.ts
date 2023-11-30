2b5d3a28caf4ccc1f6ccb8eb46f45a703086ff6a
Mahendrareddy190 <perammahendra60@gmail.com>
2023-11-30 13:40:55 +0530

    Edge cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent implements OnInit{
-  Repositories:any = [];
-  userProfile:any = {};
+export class AppComponent implements OnInit {
+  Repositories: any = [];
+  userProfile: any = {};
   p: number = 1;
-  username:string = 'johnpapa';
-  skeletonloader:boolean=true;
-  Tags:any = {}; 
-  perPageRepo:number=10;
-  constructor(
-    private apiService: ApiService
-  ) {}
-  
-  onEnter(value: string) {
-    this.username = value;
-    this.skeletonloader=true;
-    this.APIcall()
+  username: string = 'johnpapa';
+  prev_username: string = this.username;
+  skeletonloader: boolean = true;
+  Tags: any = {};
+  perPageRepo: number = 10;
+  constructor(private apiService: ApiService) {}
+
+  onEnter() {
+    if (this.prev_username != this.username) {
+      this.prev_username = this.username;
+      this.skeletonloader = true;
+      this.APIcall();
+    } else {
+      alert('Change github profile name');
+    }
   }
 
-  APIcall(){
-    this.apiService.getUser(`${this.username}`).subscribe(data => {
-      this.userProfile = data
-    }) 
-    this.apiService.getRepos(`${this.username}`).subscribe(data => {
-      this.Repositories = data      
-    })
+  APIcall() {
+    this.apiService.getUser(`${this.username}`).subscribe((data) => {
+      this.userProfile = data;
+    });
+    this.apiService.getRepos(`${this.username}`).subscribe((data) => {
+      this.Repositories = data;
+    });
     setTimeout(() => {
-      this.skeletonloader=false;
-    },  1000);
+      this.skeletonloader = false;
+    }, 1000);
     setTimeout(() => {
-      this.getCurrentPageRepos() 
-    },  1000);
-   
+      this.getCurrentPageRepos();
+    }, 1000);
   }
-  
-  getRepoTags(languages_url:string,name:string){
-      this.apiService.getTopic(languages_url).subscribe(Response => {
-      this.Tags[name] = Object.keys(Response)
-      })
+
+  getRepoTags(languages_url: string, name: string) {
+    this.apiService.getTopic(languages_url).subscribe((Response) => {
+      this.Tags[name] = Object.keys(Response);
+    });
   }
-  getCurrentPageRepos(){
-    // var i:number=0;    
-    // if(this.Tags.length!=0){this.Tags=[]}
-    if(this.Repositories.length){
-    for (const repo of this.Repositories) {
-      // if (i<this.perPageRepo) {
-        this.getRepoTags(repo.languages_url,repo.name)
-        // i+=1
-      // }
-    }
+
+  getCurrentPageRepos() {
+    if (this.Repositories.length) {
+      for (const repo of this.Repositories) {
+        this.getRepoTags(repo.languages_url, repo.name);
       }
-   }
+    }
+  }
 
   ngOnInit() {
-    this.APIcall() 
+    this.APIcall();
   }
 }
